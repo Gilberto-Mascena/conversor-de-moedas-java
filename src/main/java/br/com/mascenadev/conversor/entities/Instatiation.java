ff1e8f1cbb94eeb5a81bba3b1c8666ffe0f9c1ed
Gilberto Mascena <gilbertomascena@gmail.com>
2024-11-08 13:44:05 -0300

    :sparkles: feat: added exception handling and translated to English

--- src/main/java/br/com/mascenadev/conversor/entities/Instatiation.java
+++ src/main/java/br/com/mascenadev/conversor/entities/Instatiation.java
@@ -1,7 +1,9 @@
+
 package br.com.mascenadev.conversor.entities;
 
 import br.com.mascenadev.conversor.service.ConsumeApi;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Instatiation {
@@ -18,67 +20,74 @@ public class Instatiation {
             var menu = """
                     \n
                     ************************************************
-                     Bem vindo ao conversor de moedas!
-                        Escolha uma opção:
-                                        
-                     1 - Real ==> Dólar
-                     2 - Dólar ==> Real
-                     3 - Euro ==> Real
-                     4 - Real ==> Euro
-                     5 - Euro ==> Dólar
-                     6 - Dólar ==> Euro 
-                                                                               
-                     0 - Sair
-                    ************************************************                                     
-                    """;
-            System.out.println(menu);
-            option = sc.nextInt();
-            sc.nextLine();
+                     Welcome to the currency converter!
+                     Choose an option:
+                   \s
+                    1 - Real ==> Dollar
+                    2 - Dollar ==> Real
+                    3 - Euro ==> Real
+                    4 - Real ==> Euro
+                    5 - Euro ==> Dollar
+                    6 - Dollar ==> Euro
+                   \s
+                    0 - Exit
+                    ************************************************\s
+                   \s""";
 
-            switch (option) {
-                case 1:
-                    convert(" USD", consumeApi.getData("BRL", "USD"));
-                    break;
-                case 2:
-                    convert(" BRL", consumeApi.getData("USD", "BRL"));
-                    break;
-                case 3:
-                    convert(" BRL", consumeApi.getData("EUR", "BRL"));
-                    break;
-                case 4:
-                    convert(" EUR", consumeApi.getData("BRL", "EUR"));
-                    break;
-                case 5:
-                    convert(" USD", consumeApi.getData("EUR", "USD"));
-                    break;
-                case 6:
-                    convert(" EUR", consumeApi.getData("USD", "EUR"));
-                    break;
-                case 0:
-                    System.out.println("Saindo...");
-                    break;
-                default:
-                    System.out.println("Opção inválida! Digite uma opção válida.\n");
+            System.out.println(menu);
+            try {
+
+                option = sc.nextInt();
+                sc.nextLine();
+
+                switch (option) {
+                    case 1:
+                        convert(" USD", consumeApi.getData("BRL", "USD"));
+                        break;
+                    case 2:
+                        convert(" BRL", consumeApi.getData("USD", "BRL"));
+                        break;
+                    case 3:
+                        convert(" BRL", consumeApi.getData("EUR", "BRL"));
+                        break;
+                    case 4:
+                        convert(" EUR", consumeApi.getData("BRL", "EUR"));
+                        break;
+                    case 5:
+                        convert(" USD", consumeApi.getData("EUR", "USD"));
+                        break;
+                    case 6:
+                        convert(" EUR", consumeApi.getData("USD", "EUR"));
+                        break;
+                    case 0:
+                        System.out.println("Leaving...");
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid option! Only numbers are allowed.\n");
+                sc.nextLine();
             }
         }
     }
 
-    Double amount;
+    double amount;
 
     private void convert(String coin, Double value) {
+        try {
+            System.out.print("Enter the amount to be converted:");
+            amount = Double.parseDouble(sc.nextLine());
 
-        System.out.print("Digite o valor a ser convertido: ");
-        amount = Double.parseDouble(sc.nextLine());
-
-        if (amount > 0) {
-            double result = amount * value;
+            if (amount > 0) {
+                double result = amount * value;
+                System.out.println();
+                System.out.println("Converted value =>" + String.format("%.2f", result) + coin + "\n");
+            } else if (amount <= 0) {
+                System.out.println();
+                System.out.println("Enter a value greater than or equal to 1!\n");
+            }
+        } catch (NumberFormatException e) {
             System.out.println();
-            System.out.println("Valor convertido => " + String.format("%.2f", result) + coin + "\n");
-        }
-
-        else  {
-            System.out.println();
-            System.out.println("Digite um valor maior ou igual a um!\n");
+            System.out.println("Invalid value! Only numbers are allowed.\n");
         }
     }
 }
